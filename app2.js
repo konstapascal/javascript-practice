cafce8f60d139985e6f6fb45dbdbb529ca929be7
konstapascal <pascalkonstantinos@gmail.com>
2020-06-29 20:28:14 +0200

    Changed from global variables

--- app2.js
+++ app2.js
@@ -1,8 +1,6 @@
-import _ from './node_modules/lodash';
-
 // Arrays are object type and use reference values
-arr1 = ['elm1', 'elm2', 'elm3'];
-arr2 = arr1;
+const arr1 = ['elm1', 'elm2', 'elm3'];
+const arr2 = arr1;
 
 console.log(arr1, arr2);
 
@@ -11,8 +9,8 @@ arr1.push(2);
 console.log(arr1, arr2);
 
 // Primitive types will not
-num1 = 1;
-num2 = num1;
+let num1 = 1;
+const num2 = num1;
 
 console.log(num1, num2);
 
@@ -22,7 +20,7 @@ console.log(num1, num2);
 
 // Objects
 
-obj1 = {
+const obj1 = {
 	name: 'Konsta',
 	age: 20,
 	hobbies: [1, 2, 3],
@@ -32,14 +30,14 @@ obj1 = {
 };
 
 // Both pointing to same reference
-obj2 = obj1;
+const obj2 = obj1;
 
 // "Shallow copy" meaning objects within the object are still pointing same reference
-obj3 = { ...obj1 };
+const obj3 = { ...obj1 };
 
 // "Deep clone"
 // NOTE: stringify() will remove any functions
-obj4 = JSON.parse(JSON.stringify(obj1));
+const obj4 = JSON.parse(JSON.stringify(obj1));
 
 obj1.name = 'Changed';
 obj1.hobbies.push('pushed');
