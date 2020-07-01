dd3465aaba5f6f00b7035cd0c8f319e626a4e2f9
konstapascal <pascalkonstantinos@gmail.com>
2020-07-01 17:14:14 +0200

    -

--- app5.js
+++ app5.js
@@ -5,12 +5,18 @@
 // Prototype is an special property of a function/class/super used to create the prototype chain
 // __proto__ is a property present in ALL objects pointing up the chain
 
-obj1 = {};
-arr1 = [];
+// Global objects (Object, Array, Date, Set, Map...)
+obj1 = new Object({});
+arr1 = new Array([]);
+date1 = new Date();
+set1 = new Set([1, 2]);
+map1 = new Map();
 
 // Instance prototypes are pointing to Object prototypes
 console.log(obj1.__proto__ === Object.prototype); // true
 console.log(arr1.__proto__ === Array.prototype); // true
+console.log(date1.__proto__ === Date.prototype); // true
 
 console.log(obj1);
 console.log(arr1);
+console.log(Date.prototype);
