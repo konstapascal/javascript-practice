a1cd65845a63bbaa07d287336bad49938cafd499
konstapascal <pascalkonstantinos@gmail.com>
2020-07-12 18:14:43 +0200

    random stuff

--- app6.js
+++ app6.js
@@ -6,15 +6,18 @@ function f() {
 
 let a = 'var a';
 
-console.log(a);
-console.log(f);
+// console.log(a);
+// console.log(f);
 
-const arr = [];
+const arr = Array(2, 3);
+const arr2 = Object.create(Array);
+
 console.log(arr);
+console.log(arr.__proto__);
+console.log(arr.prototype);
+
+console.log(Array);
 
 const fun1 = new Function();
 
-console.log(fun1);
-console.dir(fun1.__proto__);
-
-console.log('test');
+console.log(f);
