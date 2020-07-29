e997c9358097d087de4fe7e301c8bf96ba5d3541
konstapascal <pascalkonstantinos@gmail.com>
2020-07-30 01:59:22 +0200

    function types

---
+++ functions.js
@@ -0,0 +1,15 @@
+// FUNCTION DECLARATION
+// function IS hoisted
+
+function function1() {}
+
+// FUNCTION EXPRESSION
+// variable hoisted but NOT initialized
+
+const function3 = function function3() {
+	console.log('function3');
+}; // named function expression
+
+const function2 = function () {}; // anon function expresssion
+
+const function4 = () => {}; // same as above, ESS6 syntax
