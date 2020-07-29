4fa6259359785490a85a0bccd1f487b0bf4acf7d
konstapascal <pascalkonstantinos@gmail.com>
2020-07-29 23:51:52 +0200

    namespace within object literal

--- app9.js
+++ app9.js
@@ -47,14 +47,30 @@ const app2Function = () => {
 
 const APP2 = app2Function();
 
+const APP3 = {
+	var1: 'var1',
+	var2: 'var2',
+	sayHello() {
+		console.log('hello from app3 ' + this.var1);
+	},
+};
+
 // console logs
 
-console.log('Output from APP1:');
-console.log(APP1.var1, APP1.var2);
-console.log(APP1.function1(), APP1.function2(), APP1.function3());
+const log = console.log;
 
-console.log('-----------------------------');
+log('Output from APP1:');
+log(APP1.var1, APP1.var2);
+log(APP1.function1(), APP1.function2(), APP1.function3());
 
-console.log('Output from APP2:');
-console.log(APP2.var1, APP2.var2);
-console.log(APP2.function1(), APP2.function2(), APP2.function3());
+log('-----------------------------');
+
+log('Output from APP2:');
+log(APP2.var1, APP2.var2);
+log(APP2.function1(), APP2.function2(), APP2.function3());
+
+log('-----------------------------');
+
+log('Output from APP3:');
+log(APP3.var1, APP3.var2);
+log(APP3.sayHello());
