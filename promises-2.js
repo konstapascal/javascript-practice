e36b05ea73a90cb9f3687a7053666c41bc6dbaf0
konstapascal <pascalkonstantinos@gmail.com>
2020-09-11 18:26:13 +0200

    promises with async/await

--- promises-2.js
+++ promises-2.js
@@ -1,23 +1,28 @@
-console.log('start');
+console.log('end 1');
 
-const p1 = new Promise(resolve => {
-	resolve('promise 1');
-});
+(async function p4() {
+	console.log('start 2');
 
-function p2() {
-	return new Promise((res, err) => {
-		res('promise 2');
+	const p1 = new Promise(resolve => {
+		resolve('promise 1');
 	});
-}
 
-function p3() {
-	return Promise.resolve('promise 3');
-}
+	function p2() {
+		return new Promise((res, err) => {
+			res('promise 2');
+		});
+	}
 
-const promisesArray = [p1, p2(), p3()];
+	function p3() {
+		return Promise.resolve('promise 3');
+	}
 
-Promise.all(promisesArray)
-	.then(results => console.log(results))
-	.catch(err => console.log(err));
+	const promisesArray = [p1, p2(), p3()];
 
-console.log('end');
+	const all = await Promise.all(promisesArray);
+	console.log(all);
+
+	console.log('end 2');
+})();
+
+console.log('end 1');
