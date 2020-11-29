1f4acfe1aef2cbea9aa7ee8169dd043fcc2eb8a2
konstapascal <pascalkonstantinos@gmail.com>
2020-11-29 02:16:46 +0100

    changes

--- this-keyword.js
+++ this-keyword.js
@@ -1,3 +1,10 @@
-const log = console.log;
+console.log('1', this);
 
-log(this);
+(function globalFunction() {
+	console.log('2', this);
+	insideFunction();
+
+	function insideFunction() {
+		console.log('3', this);
+	}
+})();
