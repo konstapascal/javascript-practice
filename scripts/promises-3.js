23a904245a2da4ae7427564ede2ef8a4ff530bb0
konstapascal <pascalkonstantinos@gmail.com>
2020-12-11 17:09:11 +0100

    changes

--- scripts/promises-3.js
+++ scripts/promises-3.js
@@ -1,11 +1,11 @@
 console.log('start');
 
-let prom1 = new Promise(resolve => {
+var prom1 = new Promise(resolve => {
 	console.log('inside prom 1');
 	resolve('resolved 1');
 });
 
-let prom2 = new Promise(resolve =>
+var prom2 = new Promise(resolve =>
 	setTimeout(() => {
 		console.log('inside prom 2');
 		resolve('resolved 2');
@@ -13,11 +13,11 @@ let prom2 = new Promise(resolve =>
 );
 
 prom2.then(() => {
-	console.log('prom 2');
+	console.log('prom 2 then');
 });
 
 prom1.then(() => {
-	console.log('prom 1');
+	console.log('prom 1 then');
 });
 
 console.log('end');
