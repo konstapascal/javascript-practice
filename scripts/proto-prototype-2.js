3a84d77bfd376e63ba9a51a4df72e62fa0e9df18
konstapascal <pascalkonstantinos@gmail.com>
2020-12-22 20:44:24 +0100

    changes

--- scripts/proto-prototype-2.js
+++ scripts/proto-prototype-2.js
@@ -3,10 +3,18 @@ function Animal(_size, _name) {
 	this.name = _name;
 }
 
+Animal.prototype.getDetails = function () {
+	console.log(`${this.name} + ${this.size}`);
+};
+
+// ------------------------------
+
 const animal_1 = new Animal(100, 'animal_1');
 const animal_2 = new Animal(200, 'animal_2');
 
 console.log(animal_1);
 console.log(animal_2);
 
-console.log(Animal.prototype.__proto__ === Object.prototype);
+console.log(animal_1.__proto__);
+console.log(animal_1.__proto__.__proto__);
+console.log(animal_1.__proto__.__proto__.__proto__);
