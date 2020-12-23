9cc8d4f90ae5fab3c2bd686d05e571805a6d34c8
konstapascal <pascalkonstantinos@gmail.com>
2020-12-23 05:55:27 +0100

    small change

--- scripts/proto-prototype-2.js
+++ scripts/proto-prototype-2.js
@@ -15,6 +15,6 @@ const animal_2 = new Animal(200, 'animal_2');
 console.log(animal_1);
 console.log(animal_2);
 
-console.log(animal_1.__proto__);
-console.log(animal_1.__proto__.__proto__);
-console.log(animal_1.__proto__.__proto__.__proto__);
+// console.log(animal_1.__proto__);
+// console.log(animal_1.__proto__.__proto__);
+// console.log(animal_1.__proto__.__proto__.__proto__);
