3dab508ff65178d0d7af45cf788af9eb920f0e5b
konstapascal <pascalkonstantinos@gmail.com>
2021-02-22 18:31:55 +0100

    how to set prototypes

--- scripts/constructor-functions.js
+++ scripts/constructor-functions.js
@@ -1,3 +1,16 @@
+function Vehicle(type, name) {
+	this.type = type;
+	this.name = name;
+}
+
+Vehicle.vehicleStaticFunction = function () {
+	console.log('I am a vehicle static function!');
+};
+
+Vehicle.prototype.vehiclePrototypeFunction = function () {
+	console.log('I am a vehicle shared function!');
+};
+
 function Car(brand, sound) {
 	this.brand = brand;
 	this.sound = sound;
@@ -6,16 +19,25 @@ function Car(brand, sound) {
 	};
 }
 
-Car.staticFunction = function () {
-	console.log('I am a static function!');
+// ALTERNATIVE 1
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car;
+
+// ALTERNATIVE 2
+// Car.prototype.__proto__ = Vehicle.prototype;
+
+//ALTERNATIVE 3
+// Object.setPrototypeOf(Car.prototype, Vehicle.prototype);
+
+Car.carStaticFunction = function () {
+	console.log('I am a car static function!');
 };
 
-Car.prototype.prototypeFunction = function () {
-	console.log('I am a shared function!');
+Car.prototype.carPrototypeFunction = function () {
+	console.log('I am a car shared function!');
 };
+
+const vehicle = new Vehicle('vehicle', 'name');
 
 const teslaCar = new Car('Tesla', 'Shhh..');
 const bmwCar = new Car('BMW', 'Vrooom..');
-
-console.log(teslaCar);
-console.log(bmwCar);
