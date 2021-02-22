b06a7e05c4325f2d87468d564a4def029eda170c
konstapascal <pascalkonstantinos@gmail.com>
2021-02-22 17:38:21 +0100

    changes

--- scripts/constructor-functions.js
+++ scripts/constructor-functions.js
@@ -14,8 +14,6 @@ Car.prototype.prototypeFunction = function () {
 	console.log('I am a shared function!');
 };
 
-const newObj = Object.create(Car.prototype);
-
 const teslaCar = new Car('Tesla', 'Shhh..');
 const bmwCar = new Car('BMW', 'Vrooom..');
 
