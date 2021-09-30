c1feabc45e537fbaa0d284fdfa8ff8a6c9a1f934
konstapascal <pascalkonstantinos@gmail.com>
2021-09-30 06:32:38 +0200

    added alphabet indexes kata

---
+++ scripts/katas/alphabet-index.js
@@ -0,0 +1,14 @@
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+// If anything in the text isn't a letter, ignore it and don't return it.
+// "a" = 1, "b" = 2, etc.
+
+function alphabetPosition(text) {
+	const alph = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+	return [...text.toLowerCase()].reduce((string, char) => {
+		!alph.includes(char) ? (string += '') : (string += `${alph.indexOf(char) + 1} `);
+		return string;
+	}, '');
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
