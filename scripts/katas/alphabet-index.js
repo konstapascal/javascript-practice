b8c85200dc886cc7d8e215865e547c3c8558c391
konstapascal <pascalkonstantinos@gmail.com>
2021-09-30 06:36:23 +0200

    added test to kata

--- scripts/katas/alphabet-index.js
+++ scripts/katas/alphabet-index.js
@@ -5,10 +5,16 @@
 function alphabetPosition(text) {
 	const alph = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
-	return [...text.toLowerCase()].reduce((string, char) => {
-		!alph.includes(char) ? (string += '') : (string += `${alph.indexOf(char) + 1} `);
-		return string;
-	}, '');
+	return [...text.toLowerCase()]
+		.reduce((string, char) => {
+			!alph.includes(char) ? (string += '') : (string += `${alph.indexOf(char) + 1} `);
+			return string;
+		}, '')
+		.trimEnd();
 }
 
-console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+// test
+console.log(
+	alphabetPosition("The sunset sets at twelve o' clock.").toString() ===
+		'20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11'
+);
