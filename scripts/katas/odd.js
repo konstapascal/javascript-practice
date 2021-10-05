a7a5eac8e59ccd3639806e383b609501062431b7
konstapascal <pascalkonstantinos@gmail.com>
2021-10-05 02:18:28 +0200

    updated odd.js

--- scripts/katas/odd.js
+++ scripts/katas/odd.js
@@ -4,16 +4,18 @@
 // [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 // [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
+const isOdd = num => (num % 2 != 0 ? true : false);
+
 function findOdd(arr) {
 	if (arr.length === 1) return arr[0];
 
 	let record = {};
-	let num;
 
 	arr.forEach(num => (!record.hasOwnProperty(num) ? (record[num] = 1) : record[num]++));
-	Object.entries(record).forEach(([key, val]) => val % 2 != 0 && (num = key));
 
-	return Number(num);
+	for (let [key, val] of Object.entries(record)) {
+		if (isOdd(val)) return Number(key);
+	}
 }
 
 console.log(findOdd([0, 1, 0, 1, 0]));
